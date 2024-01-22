9480f117810649f176bd3e5b8e02643bb902e82c
MohdTalib-twenty <abdulanas0000@gmail.com>
2024-01-22 11:56:29 +0530

    Get routes added

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const express = require("express")
 
 const app = express();
 
-
+app.get('/get',(req,res)=>{
+    res.status(200).send({
+        message:"Done"
+    })
+})
 
 
 app.listen(8000,()=>{
